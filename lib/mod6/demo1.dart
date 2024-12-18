4b4437dd052dfc8a3c98728235418fd26c9a0b8d
tneron2023 <theo.neron2023@campus-eni.fr>
2024-12-18 09:32:25 +0100

    Set State

--- lib/mod6/demo1.dart
+++ lib/mod6/demo1.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(PasswordInput());
 }
 
-class PasswordInput extends StatefulWidget{
+class PasswordInput extends StatefulWidget {
   @override
   State<PasswordInput> createState() => _PasswordInputState();
 }
 
 class _PasswordInputState extends State<PasswordInput> {
+  bool _isVisible = false;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      title: "Démo state",
+      home: Scaffold(
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                obscureText: !_isVisible,
+                decoration: InputDecoration(labelText: "Mot de passe"),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(_isVisible ? Icons.lock_open : Icons.lock),
+                Switch(
+                    value: _isVisible,
+                    onChanged: (switchValue) {
+                      setState(() {
+                        _isVisible = switchValue;
+                      });
+                    })
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
