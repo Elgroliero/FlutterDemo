8e102199d512ce680363828a4e5a0d7c43ccf278
tneron2023 <theo.neron2023@campus-eni.fr>
2024-12-19 15:02:42 +0100

    api

--- lib/mod9/demo1.dart
+++ lib/mod9/demo1.dart
@@ -1,7 +1,36 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 import 'Cat.dart';
+
+void main() {
+  runApp(DemoApi());
+}
+
+class CatView extends StatelessWidget {
+
+  Cat cat;
+
+  CatView(this.cat);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.network(cat?.urlImage ?? ''),
+        Row(
+          children: [
+            Text(
+                "Nom = ${cat?.id ?? ''}, hauteur = ${cat?.height ??
+                    ''}, largeur = ${cat?.width ?? ''}")
+          ],
+        )
+      ],
+    );
+  }
+}
+
 
 class DemoApi extends StatefulWidget {
   @override
@@ -10,6 +39,19 @@ class DemoApi extends StatefulWidget {
 
 class _DemoApiState extends State<DemoApi> {
   Cat? zouzou;
+  List<Cat> cats = [];
+
+  void getCat() async {
+    var response =
+    await http.get(Uri.parse("https://api.thecatapi.com/v1/images/search"));
+
+    if (response.statusCode == 200) {
+      var json = convert.jsonDecode(response.body);
+      setState(() {
+        zouzou = Cat.fromJson(json[0]);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +59,7 @@ class _DemoApiState extends State<DemoApi> {
       home: Scaffold(
         body: Column(
           children: [
-            ElevatedButton(onPressed: () {}, child: Text("GET CAT !")),
+            ElevatedButton(onPressed: getCat, child: Text("GET CAT !")),
             if (zouzou != null)
               Column(
                 children: [
@@ -25,11 +67,35 @@ class _DemoApiState extends State<DemoApi> {
                   Row(
                     children: [
                       Text(
-                          "Nom = ${zouzou?.id ?? ''}, hauteur = ${zouzou?.height ?? ''}, largeur = ${zouzou?.width ?? ''}"),
+                          "Nom = ${zouzou?.id ?? ''}, hauteur = ${zouzou
+                              ?.height ?? ''}, largeur = ${zouzou?.width ??
+                              ''}")
                     ],
-                  ),
+                  )
                 ],
-              )
+              ),
+            FutureBuilder<http.Response>(
+                future: http.get(Uri.parse(
+                    "https://api.thecatapi.com/v1/images/search?limit=10")),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData && snapshot.data?.body != null) {
+                    var body = snapshot.data?.body!;
+                    var json = convert.jsonDecode(body!);
+
+                    cats = List<Cat>.from(
+                        json.map((jsonCat) => Cat.fromJson(jsonCat)));
+
+                    return Expanded(child:
+                    ListView.builder(
+                        itemCount: cats.length,
+                        itemBuilder: (context, index) {
+                          return CatView(cats[index]);
+                        }
+                    )
+                    );
+                  }
+                  return CircularProgressIndicator();
+                })
           ],
         ),
       ),
